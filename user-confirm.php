dd840cba7401af767e3ec8f820eaf880a626b8fb
uetani <shiiizuka.furuta@gmail.com>
2024-10-06 11:48:11 +0900

    フォームのcss修正

--- user-confirm.php
+++ user-confirm.php
@@ -4,6 +4,7 @@ session_start();
 
 <?php include("head.php"); ?>
 <title>ユーザー確認</title>
+<link href="css/index.css" rel="stylesheet">
 </head>
 
 <body>
@@ -14,36 +15,41 @@ session_start();
 
     <main>
         <h2>入力内容確認</h2>
-        <form method="post" action="user-insert.php">
-            <table>
-                <tr>
-                    <td>名前</td>
-                    <td>
-                        <p><?= h($_POST['name']) ?></p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Login ID</td>
-                    <td>
-                        <p><?= h($_POST['lid']) ?></p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Login Password</td>
-                    <td>
-                        <p><?= h($_POST['lpw']) ?></p>
-                    </td>
-                </tr>
-            </table>
+        <div class="form-wrapper">
+            <form method="post" action="user-insert.php">
+                <table>
+                    <tr>
+                        <td>名前</td>
+                        <td>
+                            <p><?= h($_POST['name']) ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Login ID</td>
+                        <td>
+                            <p><?= h($_POST['lid']) ?></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Login Password</td>
+                        <td>
+                            <p><?= h($_POST['lpw']) ?></p>
+                        </td>
+                    </tr>
+                </table>
 
-            <!-- hidden fields to pass data to user-insert.php -->
-            <input type="hidden" name="name" value="<?= h($_POST['name']) ?>">
-            <input type="hidden" name="lid" value="<?= h($_POST['lid']) ?>">
-            <input type="hidden" name="lpw" value="<?= h($_POST['lpw']) ?>">
-
-            <input type="submit" value="登録する">
-            <button type="button" onclick="history.back()">戻る</button>
-        </form>
+                <!-- hidden fields to pass data to user-insert.php -->
+                <input type="hidden" name="name" value="<?= h($_POST['name']) ?>">
+                <input type="hidden" name="lid" value="<?= h($_POST['lid']) ?>">
+                <input type="hidden" name="lpw" value="<?= h($_POST['lpw']) ?>">
+                <div class="uc-btn-wrapper">
+                    <div class="uc-btn">
+                        <button type="submit" class="btn regibtn">登録する</button>
+                        <button type="button" class="btn" onclick="history.back()">戻る</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </main>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="js/menu.js"></script>
